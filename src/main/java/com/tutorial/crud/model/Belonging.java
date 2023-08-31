9a84a04cf9150bdbdf15a91c5ae314cd3b5459cb
Amanda~Dee <amanda_tinoco00@hotmail.com>
2023-08-31 18:24:07 -0300

    feat: add Movie model

---
+++ src/main/java/com/tutorial/crud/model/Belonging.java
@@ -0,0 +1,14 @@
+// package com.tutorial.crud.model;
+
+// import jakarta.persistence.EmbeddedId;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+
+// @Entity
+// @Table(name = "tb_belonging")
+// public class Belonging {
+
+//     @EmbeddedId
+//     private BelongingPK id = new BelongingPK();
+//     private Integer 
+// }
---
+++ src/main/java/com/tutorial/crud/model/Movie.java
@@ -0,0 +1,95 @@
+package com.tutorial.crud.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT", name = "movie_description")
+    private String description;
+
+    @Column(name = "movie_year")
+    private Integer year;
+
+    private String genre;
+    private Double rating;
+    
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+        public Movie() {
+    }
+
+    public Movie(Long id, String title, String description, Integer year, String genre, Double rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    
+}
